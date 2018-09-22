21ed76b782edaa5cc78dd6095fa27db9b98878a3
chenguangxiao <chenguangxiao@jd.com>
2018-09-23 00:02:23 +0800

    add book.vue app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 
 <script>
 import {get} from './utils'
+
 export default {
   async created () {
-    const res = await get('/weapp/demo')
-    console.log(123, res)
+    
+    // qcloud.setLoginUrl(config.loginUrl);
+    // qcloud.login({
+    //     success: function (userInfo) { 
+    //         console.log('登录成功', userInfo);
+    //     },  
+    //     fail: function (err) {
+    //         console.log('登录失败', err);
+    //     }
+    // });
+    // const res = await get('/weapp/demo')
+    // console.log(123, res)
     // wx.request({
     //     url: config.host + '/weapp/demo',
     //     success: function(res) {
@@ -12,6 +23,7 @@ export default {
     //     }
     // })
   }
+  
 }
 </script>
 
--- src/pages/books/Book.vue
+++ src/pages/books/Book.vue
@@ -1,12 +1,52 @@
 <template>
     <div>
         图书列表页面
+        <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="bindGetUserInfo" @click="getUserInfo1">获取用户信息</button>
     </div>
 </template>
 
 <script>
+import qcloud from 'wafer2-client-sdk'
 export default {
-
+  methods: {
+    getSetting(){
+      wx.getSetting({
+        success: function(res){
+          if (res.authSetting['scope.userInfo']) {
+            wx.getUserInfo({
+              success: function(res) {
+                console.log(res.userInfo)
+                //用户已经授权过
+                console.log('用户已经授权过')
+              }
+            })
+          }else{
+            console.log('用户还未授权过')
+          }
+        }
+      })
+    },
+    getUserInfo1(){
+      console.log('click事件首先触发')
+      // 判断小程序的API，回调，参数，组件等是否在当前版本可用。  为false 提醒用户升级微信版本
+      // console.log(wx.canIUse('button.open-type.getUserInfo'))
+      if(wx.canIUse('button.open-type.getUserInfo')){
+        // 用户版本可用
+      }else{
+        console.log('请升级微信版本')
+      }
+    },
+    bindGetUserInfo(e) {
+      // console.log(e.mp.detail.rawData)
+      if (e.mp.detail.rawData){
+        //用户按了允许授权按钮
+        console.log('用户按了允许授权按钮')
+      } else {
+        //用户按了拒绝按钮
+        console.log('用户按了拒绝按钮')
+      }
+    }
+  }
 }
 </script>
 
